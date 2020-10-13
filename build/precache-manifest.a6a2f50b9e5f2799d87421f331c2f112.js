3003700fef8715b705797b66a3c71a343591cc7e
pythonbuddha <pythonbuddha@gmail.com>
2020-10-13 21:18:14 +0200

    newly added files

---
+++ build/precache-manifest.a6a2f50b9e5f2799d87421f331c2f112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9caeebd4beb0c67909e9b57c207d951e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d9774d2d6aff3ff80f",
+    "url": "/static/css/main.77df4070.chunk.css"
+  },
+  {
+    "revision": "1638e05363c0baee53f5",
+    "url": "/static/js/2.e649ce4d.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.e649ce4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d9774d2d6aff3ff80f",
+    "url": "/static/js/main.7130aeb6.chunk.js"
+  },
+  {
+    "revision": "f4eb4f385d233dd24e8e",
+    "url": "/static/js/runtime-main.dab8255a.js"
+  }
+]);
